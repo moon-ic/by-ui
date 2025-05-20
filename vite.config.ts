f5d69a4c0a0c6d8a0c8f31ecbc98f402e017b348
bingyuewu <bingyuewu@tencent.com>
2025-05-20 15:00:46 +0800

    feat: 添加ts配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/packages'
+    }
+  },
   build: {
-    rollupOptions:{
-      input: './demos/index.js'
+    rollupOptions: {
+      input: './demos/index.ts' // Ensure entry file is TS
     }
   }
 })
